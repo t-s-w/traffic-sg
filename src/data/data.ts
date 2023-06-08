7ba81b26fbf3563130bb9007ba2d98c74954af60
t-s-w <tobby.satyarama@gmail.com>
2023-06-08 22:53:33 +0800

    Add rest of camera data

--- src/data/data.ts
+++ src/data/data.ts
@@ -3,25 +3,25 @@ type Mapping = Record<string, { tags: string[], name: string }>
 const tagMappings: Mapping = {
     "4703": {
         "tags": [
-            "Woodlands & Tuas Checkpoint"
+            "Tuas Checkpoint"
         ],
         "name": "Second Link at Tuas"
     },
     "4713": {
         "tags": [
-            "Woodlands & Tuas Checkpoint"
+            "Tuas Checkpoint"
         ],
         "name": "Tuas Checkpoint"
     },
     "2701": {
         "tags": [
-            "Woodlands & Tuas Checkpoint"
+            "Woodlands Checkpoint"
         ],
         "name": "Woodlands Causeway (to Johor)"
     },
     "2702": {
         "tags": [
-            "Woodlands & Tuas Checkpoint",
+            "Woodlands Checkpoint",
             "BKE"
         ],
         "name": "Woodlands Causeway (to BKE)"
@@ -46,8 +46,7 @@ const tagMappings: Mapping = {
     },
     "9703": {
         "tags": [
-            "SLE",
-            "BKE"
+            "SLE"
         ],
         "name": "SLE(BKE) Exit"
     },
@@ -95,8 +94,7 @@ const tagMappings: Mapping = {
     },
     "7794": {
         "tags": [
-            "TPE",
-            "KPE"
+            "TPE"
         ],
         "name": "TPE(KPE) Exit"
     },
@@ -105,6 +103,444 @@ const tagMappings: Mapping = {
             "TPE"
         ],
         "name": "Upp Changi Flyover"
+    },
+    "8701": {
+        "tags": [
+            "KJE"
+        ],
+        "name": "Choa Chu Kang West Flyover"
+    },
+    "8704": {
+        "tags": [
+            "KJE"
+        ],
+        "name": "Choa Chu Kang Dr"
+    },
+    "8702": {
+        "tags": [
+            "KJE"
+        ],
+        "name": "BKE Exit"
+    },
+    "8706": {
+        "tags": [
+            "KJE"
+        ],
+        "name": "Tengah Flyover"
+    },
+    "2706": {
+        "tags": [
+            "BKE"
+        ],
+        "name": "After KJE Exit"
+    },
+    "2703": {
+        "tags": [
+            "BKE"
+        ],
+        "name": "Chantek Flyover"
+    },
+    "2705": {
+        "tags": [
+            "BKE"
+        ],
+        "name": "Dairy Farm Flyover"
+    },
+    "2708": {
+        "tags": [
+            "BKE"
+        ],
+        "name": "KJE Exit (to Checkpoint)"
+    },
+    "2707": {
+        "tags": [
+            "BKE"
+        ],
+        "name": "Mandai Rd"
+    },
+    "2704": {
+        "tags": [
+            "BKE"
+        ],
+        "name": "Woodlands Flyover (to Checkpoint)"
+    },
+    "4712": {
+        "tags": [
+            "AYE"
+        ],
+        "name": "Aft Tuas West Rd"
+    },
+    "4709": {
+        "tags": [
+            "AYE"
+        ],
+        "name": "Clementi Ave 6"
+    },
+    "4716": {
+        "tags": [
+            "AYE"
+        ],
+        "name": "Benoi Rd"
+    },
+    "4707": {
+        "tags": [
+            "AYE"
+        ],
+        "name": "Jln Ahmad Ibrahim"
+    },
+    "4705": {
+        "tags": [
+            "AYE"
+        ],
+        "name": "Yuan Ching Rd"
+    },
+    "4702": {
+        "tags": [
+            "AYE"
+        ],
+        "name": "Keppel Viaduct"
+    },
+    "4704": {
+        "tags": [
+            "AYE"
+        ],
+        "name": "Lower Delta Rd"
+    },
+    "4708": {
+        "tags": [
+            "AYE"
+        ],
+        "name": "Near Dover Dr"
+    },
+    "4706": {
+        "tags": [
+            "AYE"
+        ],
+        "name": "Near NUS"
+    },
+    "4714": {
+        "tags": [
+            "AYE"
+        ],
+        "name": "West Coast Walk"
+    },
+    "4701": {
+        "tags": [
+            "AYE"
+        ],
+        "name": "To Alexandra Rd"
+    },
+    "4710": {
+        "tags": [
+            "AYE"
+        ],
+        "name": "To Pandan Gardens"
+    },
+    "4799": {
+        "tags": [
+            "Sentosa Gateway"
+        ],
+        "name": "To Sentosa"
+    },
+    "4798": {
+        "tags": [
+            "Sentosa Gateway"
+        ],
+        "name": "To Telok Blangah"
+    },
+    "1505": {
+        "tags": [
+            "MCE"
+        ],
+        "name": "Marina Blvd"
+    },
+    "1502": {
+        "tags": [
+            "MCE"
+        ],
+        "name": "Marina Coastal Drive"
+    },
+    "1501": {
+        "tags": [
+            "MCE"
+        ],
+        "name": "Maxwell Rd"
+    },
+    "1503": {
+        "tags": [
+            "MCE"
+        ],
+        "name": "MCE 1.02km"
+    },
+    "1504": {
+        "tags": [
+            "MCE"
+        ],
+        "name": "MCE/ECP"
+    },
+    "1711": {
+        "tags": [
+            "CTE"
+        ],
+        "name": "Ang Mo Kio Ave 1 Flyover"
+    },
+    "1705": {
+        "tags": [
+            "CTE"
+        ],
+        "name": "Ang Mo Kio Ave 5 Flyover"
+    },
+    "1702": {
+        "tags": [
+            "CTE"
+        ],
+        "name": "Braddell Flyover"
+    },
+    "1707": {
+        "tags": [
+            "CTE"
+        ],
+        "name": "Bt Merah Flyover"
+    },
+    "1704": {
+        "tags": [
+            "CTE"
+        ],
+        "name": "Chin Swee Rd"
+    },
+    "1709": {
+        "tags": [
+            "CTE"
+        ],
+        "name": "Bt Timah Rd Exit"
+    },
+    "1701": {
+        "tags": [
+            "CTE"
+        ],
+        "name": "Moulmein Flyover"
+    },
+    "1703": {
+        "tags": [
+            "CTE"
+        ],
+        "name": "St George Rd"
+    },
+    "1706": {
+        "tags": [
+            "CTE"
+        ],
+        "name": "Yio Chu Kang Flyover"
+    },
+    "3798": {
+        "tags": [
+            "ECP"
+        ],
+        "name": "Benjamin Sheares Bridge"
+    },
+    "3704": {
+        "tags": [
+            "ECP"
+        ],
+        "name": "Entrance from MCE"
+    },
+    "3702": {
+        "tags": [
+            "ECP"
+        ],
+        "name": "Entrance from PIE"
+    },
+    "3705": {
+        "tags": [
+            "ECP"
+        ],
+        "name": "Changi Coast Rd Exit"
+    },
+    "3793": {
+        "tags": [
+            "ECP"
+        ],
+        "name": "Laguna Flyover"
+    },
+    "3795": {
+        "tags": [
+            "ECP"
+        ],
+        "name": "Marine Parade Flyover"
+    },
+    "3796": {
+        "tags": [
+            "ECP"
+        ],
+        "name": "Tanjong Katong Flyover"
+    },
+    "3797": {
+        "tags": [
+            "ECP"
+        ],
+        "name": "Tanjong Rhu"
+    },
+    "1005": {
+        "tags": [
+            "KPE"
+        ],
+        "name": "Defu Flyover"
+    },
+    "1002": {
+        "tags": [
+            "KPE"
+        ],
+        "name": "Kallang Bahru"
+    },
+    "1004": {
+        "tags": [
+            "KPE"
+        ],
+        "name": "Kallang Way Flyover"
+    },
+    "1001": {
+        "tags": [
+            "KPE"
+        ],
+        "name": "KPE/ECP"
+    },
+    "1003": {
+        "tags": [
+            "KPE"
+        ],
+        "name": "KPE/PIE"
+    },
+    "1006": {
+        "tags": [
+            "KPE"
+        ],
+        "name": "Tampines Flyover"
+    },
+    "6705": {
+        "tags": [
+            "PIE"
+        ],
+        "name": "Adam Rd"
+    },
+    "5794": {
+        "tags": [
+            "PIE"
+        ],
+        "name": "Bedok North"
+    },
+    "6706": {
+        "tags": [
+            "PIE"
+        ],
+        "name": "BKE"
+    },
+    "6710": {
+        "tags": [
+            "PIE"
+        ],
+        "name": "Jln Anak Bukit"
+    },
+    "6711": {
+        "tags": [
+            "PIE"
+        ],
+        "name": "Entrance from ECP Changi"
+    },
+    "6713": {
+        "tags": [
+            "PIE"
+        ],
+        "name": "Simei Abe"
+    },
+    "5795": {
+        "tags": [
+            "PIE"
+        ],
+        "name": "Eunos Flyover"
+    },
+    "6712": {
+        "tags": [
+            "PIE"
+        ],
+        "name": "Exit to Clementi Ave 6"
+    },
+    "6714": {
+        "tags": [
+            "PIE"
+        ],
+        "name": "Exit to KJE"
+    },
+    "6715": {
+        "tags": [
+            "PIE"
+        ],
+        "name": "Hong Kah Flyover"
+    },
+    "5798": {
+        "tags": [
+            "PIE"
+        ],
+        "name": "Kallang Way"
+    },
+    "6701": {
+        "tags": [
+            "PIE"
+        ],
+        "name": "Kim Keat Link"
+    },
+    "6704": {
+        "tags": [
+            "PIE"
+        ],
+        "name": "Mt Pleasant"
+    },
+    "6708": {
+        "tags": [
+            "PIE"
+        ],
+        "name": "Nanyang Flyover"
+    },
+    "5797": {
+        "tags": [
+            "PIE"
+        ],
+        "name": "Paya Lebar Flyover"
+    },
+    "6703": {
+        "tags": [
+            "PIE"
+        ],
+        "name": "Thomson Flyover"
+    },
+    "6716": {
+        "tags": [
+            "PIE"
+        ],
+        "name": "Tuas Flyover"
+    },
+    "5799": {
+        "tags": [
+            "PIE"
+        ],
+        "name": "Woodsville Flyover"
+    },
+    "1113": {
+        "tags": [
+            "Changi"
+        ],
+        "name": "Tanah Merah Coast Rd towards Changi"
+    },
+    "1111": {
+        "tags": [
+            "Changi", "TPE"
+        ],
+        "name": "Exit to Loyang Ave"
+    },
+    "1112": {
+        "tags": [
+            "Changi", "TPE"
+        ],
+        "name": "Tampines Viaduct"
     }
 }
 
