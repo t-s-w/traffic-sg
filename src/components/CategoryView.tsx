897a1b5520fbdcf7ee129c820fe4712308fe107a
t-s-w <tobby.satyarama@gmail.com>
2023-06-09 16:52:20 +0800

    Add markers

--- src/components/CategoryView.tsx
+++ src/components/CategoryView.tsx
@@ -3,26 +3,11 @@ import { useParams } from 'react-router-dom';
 import type { CameraImage, SlugParams } from '../data/types.ts'
 import Camera from './Camera.tsx'
 import './style.css'
-import L from 'leaflet';
-import "leaflet/dist/leaflet.css";
-import { useEffect } from 'react';
-
+import MapView from './MapView.tsx'
 
 export default function CategoryView(props: { cameras: CameraImage[] }) {
     const { slug } = useParams<SlugParams>();
-    useEffect(() => {
-        const container = L.DomUtil.get('map');
-        if (container !== null) {
-            container._leaflet_id = null;
-        }
-        const map = L.map('map').setView([1.3521, 103.8198], 12);
-        L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.png', {
-            attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-            subdomains: 'abcd',
-            minZoom: 0,
-            maxZoom: 18
-        }).addTo(map);
-    },)
+
     if (slug === undefined) {
         return <><h1>Error: Category Not Found</h1><div id="map" style={{ display: "none" }}></div></>
     }
@@ -34,7 +19,7 @@ export default function CategoryView(props: { cameras: CameraImage[] }) {
         </>
     }
     const cameraList = props.cameras.filter((x: CameraImage) => tagMappings[x.camera_id]?.tags.includes(tag))
-    console.log(cameraList)
-    return <><div className="cameraSetContainer">{cameraList.map((x: CameraImage) => <Camera key={x.camera_id} camera={x} />)}</div>
-        <div id="map"></div></>
+
+    return <><MapView cameras={cameraList} /><div className="cameraSetContainer">{cameraList.map((x: CameraImage) => <Camera key={x.camera_id} camera={x} />)}</div>
+    </>
 }
---
+++ src/components/MapView.tsx
@@ -0,0 +1,32 @@
+import L from 'leaflet';
+import "leaflet/dist/leaflet.css";
+import { useEffect } from 'react';
+import { CameraImage } from '../data/types.ts'
+
+const mapOptions = {
+    scrollWheelZoom: false,
+    touchZoom: false,
+    dragging: false,
+    boxZoom: false
+}
+
+export default function MapView(props: { cameras: CameraImage[] }) {
+    const { cameras } = props;
+    useEffect(() => {
+        const container = L.DomUtil.get('map');
+        if (container !== null) {
+            container._leaflet_id = null;
+        }
+        const map = L.map('map', mapOptions).setView([1.3521, 103.8198], 11);
+        L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.png', {
+            attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            subdomains: 'abcd',
+            minZoom: 0,
+            maxZoom: 18
+        }).addTo(map);
+        cameras.forEach(x => {
+            L.marker([x.location.latitude, x.location.longitude]).addTo(map);
+        })
+    }, [cameras])
+    return cameras.length ? <div id="map"></div> : <div id="map" style={{ display: 'none' }}></div>
+}
