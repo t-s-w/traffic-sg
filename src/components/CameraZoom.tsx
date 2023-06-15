707d048126eacdfa66e2f596c2fecae92bd441af
t-s-w <tobby.satyarama@gmail.com>
2023-06-15 16:00:42 +0800

    Improve formatting of date and location on camera zoom

--- src/components/CameraZoom.tsx
+++ src/components/CameraZoom.tsx
@@ -14,13 +14,15 @@ export default function CameraZoom(props: { cameras: CameraImage[] }) {
     if (!camera) {
         return <h1> Error! Camera ID not found </h1>
     }
+    const timestamp = new Date(camera.timestamp)
+
     return <div className="cameraDetails">
         <h2>{tagMappings[cameraId].name}</h2>
-        <Link to="#" onClick={(evt) => {evt.preventDefault; navigate(-1)}}>
-        <img src={camera.image} className="cameraImage" />
+        <Link to="#" onClick={(evt) => { evt.preventDefault; navigate(-1) }}>
+            <img src={camera.image} className="cameraImage" />
         </Link>
-        <p><span className="infoLabel">Time of capture: </span>{camera.timestamp}</p>
-        <p><span className="infoLabel">Geo coordinates: </span>{`[${camera.location.latitude} ${camera.location.longitude}]`}</p>
-        <Link to="#" className="navLink" onClick={(evt) => {evt.preventDefault; navigate(-1)}}>Back</Link>
+        <p><span className="infoLabel">Time of capture: </span>{timestamp.toLocaleDateString('en-sg', { year: "numeric", month: "long", day: "numeric", hour: "numeric", minute: "numeric" })}</p>
+        <p><span className="infoLabel">Geo coordinates: </span>{`[${camera.location.latitude}, ${camera.location.longitude}]`}</p>
+        <Link to="#" className="navLink" onClick={(evt) => { evt.preventDefault; navigate(-1) }}>Back</Link>
     </div>
 }
